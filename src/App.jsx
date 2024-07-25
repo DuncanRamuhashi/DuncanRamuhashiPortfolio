cef03ae9262f4c79a0453d729e31b2759ae13706
Duncan Ramuhashi <duncanramohashi@gmail.com>
2024-07-25 15:57:03 +0200

    done

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+import CoverSetter from './CoverSetter';
+import AboutPage from './AboutPage';
+import ExWork from './ExWork';
+import Activity from './Activity';
+import Work from './Work';
+import ContactPage from './ContactPage';
+import Footer from './Footer';
+import Projects from './Projects';
+function App() {
+  
+
+  return (
+    <>
+   
+      <CoverSetter/>
+      <section id="AboutPage"><AboutPage/></section>
+      <section id="ExWork"><ExWork/></section>
+      <section id="Activity"> <Activity/></section>
+      <section id="Work"><Work/></section>
+      <section id="ContactPage"><ContactPage/></section>
+      <section id="Footer"><Footer/></section>
+
+     
+      
+     
+      
+      
+      
+    </>
+  );
+}
+
+export default App
